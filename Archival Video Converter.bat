7a6d6787b8344348e12c22f29241dce60d5b12bf
Alpar Duman <83065389+AlparDuman@users.noreply.github.com>
2025-08-22 17:53:01 +0200

    Update Archival Video Converter.bat
    1) Internal config for preset user input, allowing for automation
    2) Added encoder options for libsvtav1, h264_amf, hevc_amf, h264_sqv & hevc_qsv
    3) Table shows subjective stats instead of results for test video file
    4) FFprobe is now belownormal CPU priority in background
    5) Add support for 10bit video encoding
    6) On Error console text will be red & wait for user action, on continue back to normal

--- Archival Video Converter.bat
+++ Archival Video Converter.bat
@@ -5,6 +5,38 @@ setlocal enabledelayedexpansion
 
 
 
+Rem ==========[ Config ]==========
+
+Rem Override userDelete variable and skip input
+Rem "userDeletion=" Ask for user input
+Rem "userDeletion=2" Skip user input and pretend user input us 2
+set "userDeletion="
+
+Rem Override userCodec variable and skip input
+Rem "userCodec=" Ask for user input
+Rem "userCodec=4" Skip user input and pretend user input us 4
+set "userCodec="
+
+Rem =======[ Config Check ]=======
+
+echo "   1 2 " | find " !userDeletion! " >nul
+if errorlevel 1 (
+    echo Invalid preset for userDeletion!
+	timeout /t 999
+	exit 1
+)
+
+echo "   1 2 3 4 5 6 7 8 9 " | find " !userCodec! " >nul
+if errorlevel 1 (
+    echo Invalid preset for userCodec!
+	timeout /t 999
+	exit 1
+)
+
+Rem ========[ Config End ]========
+
+
+
 Rem	Copyright (C) 2025 Alpar Duman
 Rem	This file is part of archival-video-converter-batch.
 Rem	
@@ -52,35 +84,44 @@ rem Ask user what to do with original video file after successful convertion
 echo What to do with original video file after successful convertion:
 echo [1] Keep original video file
 echo [2] Delete original video file
-set /p userDeletion=Select number: 
+
+if "!userDeletion!"=="" (
+	set /p userDeletion=Select number: 
+) else (
+	echo Select number: !userDeletion! ^(from config^)
+)
 echo.
 
-if not "!userDeletion!"=="1" (
-	if not "!userDeletion!"=="2" (
-		cls
-		goto :init
-	)
+echo " 1 2 " | find " !userDeletion! " >nul
+if errorlevel 1 (
+    cls
+    goto :init
 )
 
 rem Ask user video codec
-echo Which video codec is prefered:
-echo     ^| Device     ^| Codec ^| Speed ^| Quality ^| Size  ^| Compatibility
-echo ----+------------+-------+-------+---------+-------+--------------
-echo [1] ^| any CPU    ^| x264  ^| base  ^| best    ^| base  ^| best
-echo [2] ^| any CPU    ^| x265  ^| x0.09 ^| best    ^| x0.79 ^| most
-echo [3] ^| Nvidia GPU ^| h264  ^| x8.5  ^| good    ^| x3.25 ^| best
-echo [4] ^| Nvidia GPU ^| h265  ^| x9    ^| good    ^| x1.5  ^| most
-set /p userCodec=Select number: 
+echo Which video codec is prefered and supported:
+echo     ^| Device     ^| Codec  ^| Speed  ^| Quality ^| Size   ^| Compatibility
+echo ----+------------+--------+--------+---------+--------+--------------
+echo [1] ^| CPU ^(any^)  ^| x264   ^| medium ^| best    ^| medium ^| best
+echo [2] ^| CPU ^(any^)  ^| x265   ^| slower ^| best    ^| small  ^| good
+echo [3] ^| CPU ^(any^)  ^| svtav1 ^| slow   ^| best    ^| small  ^| medium
+echo [4] ^| GPU Nvidia ^| h264   ^| fast   ^| better  ^| big    ^| better
+echo [5] ^| GPU Nvidia ^| h265   ^| fast   ^| better  ^| medium ^| good
+echo [6] ^| GPU Amd    ^| h264   ^| fast   ^| good    ^| big    ^| better
+echo [7] ^| GPU Amd    ^| h265   ^| fast   ^| good    ^| medium ^| good
+echo [8] ^| GPU Intel  ^| h264   ^| fast   ^| medium  ^| big    ^| better
+echo [9] ^| GPU Intel  ^| h265   ^| medium ^| medium  ^| medium ^| good
 
-if not "!userCodec!"=="1" (
-	if not "!userCodec!"=="2" (
-		if not "!userCodec!"=="3" (
-			if not "!userCodec!"=="4" (
-				cls
-				goto :init
-			)
-		)
-	)
+if "!userCodec!"=="" (
+	set /p userCodec=Select number: 
+) else (
+	echo Select number: !userCodec! ^(from config^)
+)
+
+echo " 1 2 3 4 5 6 7 8 9 " | find " !userCodec! " >nul
+if errorlevel 1 (
+    cls
+    goto :init
 )
 
 rem Go through each argument, could be a folder or a file
@@ -108,6 +149,7 @@ exit /b 0
 
 
 :process_file
+
 echo.
 
 rem Escape file name, path & extension
@@ -115,38 +157,43 @@ set "filePathNameExtension=%~dpnx1"
 set "filePathName=%~dpn1"
 set "fileName=%~n1"
 
+rem Input & wip file path and name
+set "input=!filePathNameExtension!"
+set "wip=!filePathName!.!userCodec!_wip.mp4"
+set "archival=!fileName!.!userCodec!_archive.mp4"
+
 rem Skip if output already exists
 if exist "!filePathName!.archive.mp4" (
-	echo Skip !filePathNameExtension!
+	echo Skip !input!
 	exit /b 0
 )
 
 rem Check if file is not from previous Run
 if not "!fileName!"=="!fileName:.wip=!" (
-	echo Skip !filePathNameExtension!
+	echo Skip !input!
 	exit /b 0
 )
 
 if not "!fileName!"=="!fileName:.archive=!" (
-	echo Skip !filePathNameExtension!
+	echo Skip !input!
 	exit /b 0
 )
 
 rem Detect audio & video streams
-echo Analyse !filePathNameExtension!
+echo Analyse !input!
 set "has_audio=0"
 set "has_video=0"
 
-for /f "delims=" %%A in ('ffprobe -v quiet -show_entries stream^=codec_type -of default^=nw^=1:nk^=1 "!filePathNameExtension!" 2^>nul') do (
+for /f "delims=" %%A in ('start "" /b /belownormal /wait ffprobe -v quiet -show_entries stream^=codec_type -of default^=nw^=1:nk^=1 "!input!" 2^>nul') do (
 	if /i "%%A"=="audio" (
 		set "has_audio=1"
 	)
 	if /i "%%A"=="video" (
-		for /f "tokens=1" %%B in ('ffprobe -v quiet -select_streams v:0 -show_entries stream^=nb_frames -of default^=nokey^=1:noprint_wrappers^=1 "!filePathNameExtension!" 2^>nul') do (
+		for /f "tokens=1" %%B in ('start "" /b /belownormal /wait ffprobe -v quiet -select_streams v:0 -show_entries stream^=nb_frames -of default^=nokey^=1:noprint_wrappers^=1 "!input!" 2^>nul') do (
 			set "has_video=%%B"
 		)
 		if "!has_video!"=="N/A" (
-			for /f "tokens=1" %%B in ('ffprobe -v quiet -select_streams v:0 -count_frames -show_entries stream^=nb_read_frames -of default^=nokey^=1:noprint_wrappers^=1 "!filePathNameExtension!" 2^>nul') do (
+			for /f "tokens=1" %%B in ('start "" /b /belownormal /wait ffprobe -v quiet -select_streams v:0 -count_frames -show_entries stream^=nb_read_frames -of default^=nokey^=1:noprint_wrappers^=1 "!input!" 2^>nul') do (
 				set "has_video=%%B"
 			)
 		)
@@ -169,49 +216,77 @@ if !has_video! leq 1 (
 	exit /b 0
 )
 
-rem Input & wip file path and name
-set "input=!filePathNameExtension!"
-set "wip=!filePathNameExtension!.wip.mp4"
+rem Detect bitdepth of video stream
+set "pix_fmt=yuv420p"
+for /f "tokens=*" %%a in ('start "" /b /belownormal /wait ffprobe -v error -select_streams v:0 -show_entries stream^=bits_per_raw_sample -of default^=noprint_wrappers^=1:nokey^=1 "!input!" 2^>nul') do (
+	if "%%a"=="10" (
+		set "pix_fmt=yuv420p10le"
+	)
+)
 
+rem Prepare query of same arguments
+set "query="" /b /belownormal /wait ffmpeg -hide_banner -y -v error -stats -i "!input!" -movflags +faststart -map 0:v -fps_mode vfr -map 0:a -c:a aac -b:a 192k -tag:a mp4a -pix_fmt !pix_fmt! -tag:v"
 
-rem Run FFmpeg encoding (adjust command as needed)
+rem Run FFmpeg encoding
 if "!userCodec!"=="1" (
-	start "" /b /belownormal /wait ffmpeg -hide_banner -y -v error -stats -i "%input%" -movflags +faststart -map 0:v -fps_mode vfr -map 0:a -c:a aac -b:a 192k -tag:a mp4a -crf 18 -tag:v avc1 -c:v libx264 -pix_fmt yuv420p -preset placebo -x264-params ref=4 "%wip%"
+	start !query! avc1 -c:v libx264 -crf 18 -preset placebo -x264-params ref=4 "!wip!"
 )
 if "!userCodec!"=="2" (
-	start "" /b /belownormal /wait ffmpeg -hide_banner -y -v error -stats -i "%input%" -movflags +faststart -map 0:v -fps_mode vfr -map 0:a -c:a aac -b:a 192k -tag:a mp4a -crf 23 -tag:v hvc1 -c:v libx265 -pix_fmt yuv420p -preset placebo -x265-params ref=4:log-level=error "%wip%"
+	start !query! hvc1 -c:v libx265 -crf 23 -preset placebo -x265-params ref=4:log-level=error "!wip!"
 )
 if "!userCodec!"=="3" (
-	start "" /b /belownormal /wait ffmpeg -hide_banner -y -v error -stats -i "%input%" -movflags +faststart -map 0:v -fps_mode vfr -map 0:a -c:a aac -b:a 192k -tag:a mp4a -cq 18 -tag:v avc1 -c:v h264_nvenc -pix_fmt yuv420p -preset p7 -rc vbr "%wip%"
+	set SVT_LOG=1
+	start !query! av01 -c:v libsvtav1 -crf 28 -preset 2 "!wip!"
 )
 if "!userCodec!"=="4" (
-	start "" /b /belownormal /wait ffmpeg -hide_banner -y -v error -stats -i "%input%" -movflags +faststart -map 0:v -fps_mode vfr -map 0:a -c:a aac -b:a 192k -tag:a mp4a -cq 23 -tag:v hvc1 -c:v hevc_nvenc -pix_fmt yuv420p -preset p7 -rc vbr "%wip%"
+	start !query! avc1 -c:v h264_nvenc -cq 18 -preset p7 -rc vbr "!wip!"
 )
+if "!userCodec!"=="5" (
+	start !query! hvc1 -c:v hevc_nvenc -cq 23 -preset p7 -rc vbr "!wip!"
+)
+if "!userCodec!"=="6" (
+	start !query! avc1 -c:v h264_amf -rc cqp -cqp 18 -quality_best "!wip!"
+)
+if "!userCodec!"=="7" (
+	start !query! hvc1 -c:v hevc_amf -rc cqp -cqp 23 -quality_best "!wip!"
+)
+if "!userCodec!"=="8" (
+	start !query! avc1 -c:v h264_qsv -global_quality 18 -preset 1 -look_ahead 1 "!wip!"
+)
+if "!userCodec!"=="9" (
+	start !query! hvc1 -c:v hevc_qsv -global_quality 23 -preset 1 -look_ahead 1 "!wip!"
+)
+
 if not errorlevel 0 (
-	del "%wip%"
-    echo Encoding failed.
+	del "!wip!"
+	color 0C
+    echo Encoding failed
     pause
+	color 07
     exit /b 1
 )
 
 rem Rename .wip to .archive
-ren "%wip%" "!fileName!.archive.mp4"
+ren "!wip!" "!archival!"
 if not errorlevel 0 (
-	del "%wip%"
-    echo Renaming failed.
+	del "!wip!"
+	color 0C
+    echo Renaming failed
     pause
+	color 07
     exit /b 1
 )
 
 rem Delete input file
 if "!userDeletion!"=="2" (
-	del /f "%input%"
+	del /f "!input!"
 	if not errorlevel 0 (
-		echo Failed to delete source file.
+		color 0C
+		echo Failed to delete source file
 		pause
+		color 07
 		exit /b 1
 	)
 )
 
 exit /b 0
-
