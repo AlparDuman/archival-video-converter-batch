d0bf4b61a49559f03c888c861c76fd87eb5a60d5
Alpar Duman <83065389+AlparDuman@users.noreply.github.com>
2025-11-09 23:40:54 +0100

    Update Archival Video Converter.bat
    rework

--- Archival Video Converter.bat
+++ Archival Video Converter.bat
@@ -54,13 +54,16 @@ rem	along with archival-video-converter-batch. If not, see
 rem	<https://github.com/AlparDuman/archival-video-converter-batch/blob/main/LICENSE>
 rem	else <https://www.gnu.org/licenses/>.
 
+set "version=v1.1"
+set "url=https://github.com/AlparDuman/archival-video-converter-batch"
+
 :init
 echo     _             _     _            _  __     ___     _               ____                          _            
 echo    / \   _ __ ___^| ^|__ (_)_   ____ _^| ^| \ \   / (_) __^| ^| ___  ___    / ___^|___  _ ____   _____ _ __^| ^|_ ___ _ __ 
 echo   / _ \ ^| '__/ __^| '_ \^| \ \ / / _` ^| ^|  \ \ / /^| ^|/ _` ^|/ _ \/ _ \  ^| ^|   / _ \^| '_ \ \ / / _ \ '__^| __/ _ \ '__^|
 echo  / ___ \^| ^| ^| (__^| ^| ^| ^| ^|\ V / (_^| ^| ^|   \ V / ^| ^| (_^| ^|  __/ (_) ^| ^| ^|__^| (_) ^| ^| ^| \ V /  __/ ^|  ^| ^|^|  __/ ^|   
 echo /_/   \_\_^|  \___^|_^| ^|_^|_^| \_/ \__,_^|_^|    \_/  ^|_^|\__,_^|\___^|\___/   \____\___/^|_^| ^|_^|\_/ \___^|_^|   \__\___^|_^|   
-echo v1.0 ================================================ https://github.com/AlparDuman/archival-video-converter-batch
+echo !version! ================================================ !url!
 echo.
 
 
@@ -84,12 +87,13 @@ where ffprobe >nul 2>&1 || (
 
 rem hint for internal config
 echo ^| To skip user input, edit internal config.
+echo ^| Only video and audio streams are encoded, other metadata is ignored.
 echo ^|
 
 rem Ask user what to do with original video file after successful convertion
 echo ^| What to do with original video file after successful convertion:
-echo ^| [1] Keep original video file
-echo ^| [2] Delete original video file
+echo ^| [1] Keep
+echo ^| [2] Delete
 
 if "!userDeletion!"=="" (
 	set /p userDeletion=^| Select number: 
@@ -105,7 +109,7 @@ if errorlevel 1 (
 )
 
 rem Ask user video codec
-echo ^| Which video codec is prefered and supported:
+echo ^| Select a supported video codec:
 echo ^|     ^| Device     ^| Codec  ^| Speed  ^| Quality ^| Size   ^| Compatibility
 echo ^| ----+------------+--------+--------+---------+--------+--------------
 echo ^| [1] ^| CPU ^(any^)  ^| x264   ^| medium ^| best    ^| medium ^| best
@@ -195,74 +199,71 @@ for /f "delims=" %%A in ('start "" /b /belownormal /wait ffprobe -v quiet -show_
 		set "has_audio=1"
 	)
 	if /i "%%A"=="video" (
-		for /f "tokens=1" %%B in ('start "" /b /belownormal /wait ffprobe -v quiet -select_streams v:0 -show_entries stream^=nb_frames -of default^=nokey^=1:noprint_wrappers^=1 "!input!" 2^>nul') do (
-			set "has_video=%%B"
-		)
-		if "!has_video!"=="N/A" (
-			for /f "tokens=1" %%B in ('start "" /b /belownormal /wait ffprobe -v quiet -select_streams v:0 -count_frames -show_entries stream^=nb_read_frames -of default^=nokey^=1:noprint_wrappers^=1 "!input!" 2^>nul') do (
-				set "has_video=%%B"
-			)
-		)
+		set "has_video=1"
 	)
 )
 
-rem Check for audio
+rem Count frames for mute videos
 if !has_audio! equ 0 (
-	echo Skip, not a video file
-	exit /b 0
-)
-
-rem Check for images
-for /f "delims=0123456789" %%A in ("!has_video!") do (
-	echo Skip, not a video file
-	exit /b 0
-)
-if !has_video! leq 1 (
-	echo Skip, not a video file
-	exit /b 0
+	for /f "tokens=1" %%B in ('start "" /b /belownormal /wait ffprobe -v quiet -select_streams v:0 -show_entries stream^=nb_frames -of default^=nokey^=1:noprint_wrappers^=1 "!input!" 2^>nul') do (
+		set "has_video=%%B"
+	)
+	
+	if "!has_video!"=="N/A" (
+		for /f "tokens=1" %%B in ('start "" /b /belownormal /wait ffprobe -v quiet -select_streams v:0 -count_frames -show_entries stream^=nb_read_frames -of default^=nokey^=1:noprint_wrappers^=1 "!input!" 2^>nul') do (
+			set "has_video=%%B"
+		)
+	)
+	
+	rem Check for video
+	for /f "delims=0123456789" %%A in ("!has_video!") do (
+		echo Skip, not a video file
+		exit /b 0
+	)
+	
+	if !has_video! leq 1 (
+		echo Skip, not a video file
+		exit /b 0
+	)
+) else (
+	rem Check for video
+	if !has_video! equ 0 (
+		echo Skip, not a video file
+		exit /b 0
+	)
 )
 
 rem Detect bitdepth of video stream
 set "pix_fmt=yuv420p"
 for /f "tokens=*" %%a in ('start "" /b /belownormal /wait ffprobe -v error -select_streams v:0 -show_entries stream^=bits_per_raw_sample -of default^=noprint_wrappers^=1:nokey^=1 "!input!" 2^>nul') do (
-	if "%%a"=="10" (
-		set "pix_fmt=yuv420p10le"
-	)
+	if "%%a"=="10" set "pix_fmt=yuv420p10le"
 )
 
-rem Prepare query of same arguments
-set "query="" /b /belownormal /wait ffmpeg -hide_banner -y -v error -stats -i "!input!" -movflags +faststart -map 0:v -fps_mode vfr -map 0:a -c:a aac -b:a 192k -tag:a mp4a -pix_fmt !pix_fmt! -tag:v"
+rem Enable log for svt
+set SVT_LOG=1
 
-rem Run FFmpeg encoding
-if "!userCodec!"=="1" (
-	start !query! avc1 -c:v libx264 -crf 18 -preset placebo -x264-params ref=4 "!wip!"
-)
-if "!userCodec!"=="2" (
-	start !query! hvc1 -c:v libx265 -crf 23 -preset placebo -x265-params ref=4:log-level=error "!wip!"
-)
-if "!userCodec!"=="3" (
-	set SVT_LOG=1
-	start !query! av01 -c:v libsvtav1 -crf 28 -preset 2 "!wip!"
-)
-if "!userCodec!"=="4" (
-	start !query! avc1 -c:v h264_nvenc -cq 18 -preset p7 -rc vbr "!wip!"
-)
-if "!userCodec!"=="5" (
-	start !query! hvc1 -c:v hevc_nvenc -cq 23 -preset p7 -rc vbr "!wip!"
-)
-if "!userCodec!"=="6" (
-	start !query! avc1 -c:v h264_amf -rc cqp -cqp 18 -quality_best "!wip!"
-)
-if "!userCodec!"=="7" (
-	start !query! hvc1 -c:v hevc_amf -rc cqp -cqp 23 -quality_best "!wip!"
-)
-if "!userCodec!"=="8" (
-	start !query! avc1 -c:v h264_qsv -global_quality 18 -preset 1 -look_ahead 1 "!wip!"
-)
-if "!userCodec!"=="9" (
-	start !query! hvc1 -c:v hevc_qsv -global_quality 23 -preset 1 -look_ahead 1 "!wip!"
-)
+rem encode with cpu
+if "!userCodec!"=="1" set "query=-map 0:v -c:v libx264 -tag:v avc1 -crf 18 -preset placebo -x264-params ref=4:log-level=error"
+if "!userCodec!"=="2" set "query=-map 0:v -c:v libx265 -tag:v hvc1 -crf 23 -preset placebo -x265-params ref=4:log-level=error"
+if "!userCodec!"=="3" set "query=-map 0:v -c:v libsvtav1 -tag:v av01 -crf 28 -preset 2"
+rem encode with nvidia
+if "!userCodec!"=="4" set "query=-map 0:v -c:v h264_nvenc -tag:v avc1 -cq 18 -preset p7 -rc vbr"
+if "!userCodec!"=="5" set "query=-map 0:v -c:v hevc_nvenc -tag:v hvc1 -cq 23 -preset p7 -rc vbr"
+rem encode with amd
+if "!userCodec!"=="6" set "query=-map 0:v -c:v h264_amf -tag:v avc1 -rc cqp -cqp 18 -quality_best"
+if "!userCodec!"=="7" set "query=-map 0:v -c:v hevc_amf -tag:v hvc1 -rc cqp -cqp 23 -quality_best"
+rem encode with intel
+if "!userCodec!"=="8" set "query=-map 0:v -c:v h264_qsv -tag:v avc1 -global_quality 18 -preset 1 -look_ahead 1"
+if "!userCodec!"=="9" set "query=-map 0:v -c:v hevc_qsv -tag:v hvc1 -global_quality 23 -preset 1 -look_ahead 1"
 
+rem finish query
+set "query=!query! -fps_mode cfr -g 60 -map 0:a? -c:a aac -tag:a mp4a -b:a 192k -pix_fmt !pix_fmt! -movflags +faststart"
+set "query=-metadata comment="!version! !url! !query!" !query!"
+
+rem do encoding
+start "" /b /belownormal /wait ffmpeg -hide_banner -y -v error -stats -i "!input!" !query! "!wip!"
+
+rem on error
 if not errorlevel 0 (
 	del "!wip!"
 	color 0C
